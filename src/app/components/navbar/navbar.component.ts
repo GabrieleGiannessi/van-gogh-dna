52ecba2f2b1231ca9c6831b46a504ff596af9a11
Gabriele <gabrielegiannessi@gmail.com>
2025-06-23 11:31:39 +0200

    update navbar links

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,18 @@
 import { Component, computed, inject, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgbCollapseModule, RouterLink],
+  imports: [NgbCollapseModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
   authService = inject(AuthService);
+  router = inject(Router)
 
   collapse = signal<boolean>(true);
   isMenuCollapsed = computed(() => this.collapse());
